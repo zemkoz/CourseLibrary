33755eebde8bb3d0abbdf0d171e0479994b14c8c
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-28 11:11:19 +0200

    Align URIs to RESTFul API guidelines

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CourseLibrary.API.Controllers;
 
 [ApiController] 
+[Route("api/authors")]
 public class AuthorsController : ControllerBase
 {
     private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -22,33 +23,26 @@ public class AuthorsController : ControllerBase
             throw new ArgumentNullException(nameof(mapper));
     }
 
-    [HttpPost("api/author")] 
+    [HttpGet] 
     public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAuthors()
     { 
-        // get authors from repo
         var authorsFromRepo = await _courseLibraryRepository
             .GetAuthorsAsync(); 
-
-        // return them
         return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
     }
 
-    [HttpGet("api/getauthor/{authorId}", Name = "GetAuthor")]
+    [HttpGet("{authorId}")]
     public async Task<ActionResult<AuthorDto>> GetAuthor(Guid authorId)
     {
-        // get author from repo
         var authorFromRepo = await _courseLibraryRepository.GetAuthorAsync(authorId);
-
         if (authorFromRepo == null)
         {
             return NotFound();
         }
-
-        // return author
         return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
     }
 
-    [HttpPost("api/authors")]
+    [HttpPost]
     public async Task<ActionResult<AuthorDto>> CreateAuthor(AuthorDto author)
     {
         var authorEntity = _mapper.Map<Entities.Author>(author);
