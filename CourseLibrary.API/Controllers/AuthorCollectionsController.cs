06a49d89288697f790a8cf549dde275a017e8f1e
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-28 15:45:24 +0200

    Create endpoint for creating collection of authors.

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers;
+
+[ApiController]
+[Route("api/authorcollections")]
+public class AuthorCollectionsController : ControllerBase
+{
+    private readonly ICourseLibraryRepository _courseLibraryRepository;
+    private readonly IMapper _mapper;
+
+    public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+    {
+        _courseLibraryRepository = courseLibraryRepository 
+                                   ?? throw new ArgumentException(nameof(courseLibraryRepository));
+        _mapper = 
+            mapper ?? throw new ArgumentException(nameof(mapper));
+    }
+
+    [HttpGet("({authorIds})", Name = "GetAuthorCollection")]
+    public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAuthorCollection(
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] [FromRoute] IEnumerable<Guid> authorIds)
+    {
+        var authorEntities = await _courseLibraryRepository.GetAuthorsAsync(authorIds);
+        if (authorIds.Count() != authorEntities.Count())
+        {
+            return NotFound();
+        }
+        
+        var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+        return Ok(authorsToReturn);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(
+        IEnumerable<AuthorForCreationDto> authorCollection)
+    {
+        var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
+        foreach (var author in authorEntities)
+        {
+            _courseLibraryRepository.AddAuthor(author);
+        }
+        await _courseLibraryRepository.SaveAsync();
+
+        var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+        var authorIds = string.Join(
+            ",",
+            authorCollectionToReturn.Select(a => a.Id)
+        );
+        
+        return CreatedAtAction("GetAuthorCollection", 
+            new { authorIds },
+            authorCollectionToReturn);
+    }
+}
--- CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -6,4 +6,6 @@ public class AuthorForCreationDto
     public string LastName { get; set; } = string.Empty;
     public DateTimeOffset DateOfBirth { get; set; }
     public string MainCategory { get; set; } = string.Empty;
+    public ICollection<CourseForCreationDto> courses { get; set; }
+        = new List<CourseForCreationDto>();
 }
