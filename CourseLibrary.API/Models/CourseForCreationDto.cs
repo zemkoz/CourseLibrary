125563181df1dfeee3182c374e9a5d98c14b5920
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-30 10:51:40 +0200

    Add annotation based validation rules for DTOs used for create and update a course.

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,8 +1,13 @@
-﻿namespace CourseLibrary.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models;
 
 public class CourseForCreationDto
 {
+    [Required(ErrorMessage = "You should fill out a title.")]
+    [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
     public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
     public string Description { get; set; } = string.Empty;
 }
-
--- CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -1,8 +1,14 @@
-﻿namespace CourseLibrary.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models;
 
 public class CourseForUpdateDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(1500)]
     public string Description { get; set; } = string.Empty;
 }
 
